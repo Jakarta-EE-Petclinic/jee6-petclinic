4eff104df1300c4b2d7e59c395845dd76357d50a
phasenraum2010 <thomas@woehlke.org>
2014-01-21 13:29:59 +0100

    added webtests

--- src/test/java/org/woehlke/jee6/petclinic/web/Deployments.java
+++ src/test/java/org/woehlke/jee6/petclinic/web/Deployments.java
@@ -6,6 +6,8 @@ import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.importer.ExplodedImporter;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.jboss.shrinkwrap.resolver.api.maven.Maven;
+import org.woehlke.jee6.petclinic.dao.PetTypeDao;
+import org.woehlke.jee6.petclinic.dao.PetTypeDaoImpl;
 import org.woehlke.jee6.petclinic.dao.SpecialtyDao;
 import org.woehlke.jee6.petclinic.dao.SpecialtyDaoImpl;
 import org.woehlke.jee6.petclinic.entities.*;
@@ -28,6 +30,23 @@ public class Deployments {
         return ShrinkWrap.create(WebArchive.class, "specialties.war")
                 .addClasses(SpecialtyController.class, LanguageBean.class,
                         SpecialtyDao.class, SpecialtyDaoImpl.class,
+                        Owner.class, Pet.class, PetType.class,
+                        Specialty.class, Vet.class, Visit.class)
+                .merge(ShrinkWrap.create(GenericArchive.class).as(ExplodedImporter.class)
+                        .importDirectory(WEBAPP_SRC).as(GenericArchive.class),
+                        "/", Filters.include(".*\\.xhtml$"))
+                .addAsResource("META-INF/persistence.xml")
+                .addAsResource("messages_de.properties")
+                .addAsResource("messages_en.properties")
+                .addAsLibraries(deps)
+                .setWebXML("WEB-INF/web.xml");
+    }
+
+    public static WebArchive createPetTypeDeployment() {
+        File[] deps = Maven.resolver().loadPomFromFile("pom.xml").importRuntimeDependencies().resolve().withTransitivity().asFile();
+        return ShrinkWrap.create(WebArchive.class, "pettypes.war")
+                .addClasses(PetTypeController.class, LanguageBean.class,
+                        PetTypeDao.class, PetTypeDaoImpl.class,
                         Owner.class, Pet.class, PetType.class,
                         Specialty.class, Vet.class, Visit.class)
                 .merge(ShrinkWrap.create(GenericArchive.class).as(ExplodedImporter.class)
---
+++ src/test/java/org/woehlke/jee6/petclinic/web/PetTypeTest.java
@@ -0,0 +1,128 @@
+package org.woehlke.jee6.petclinic.web;
+
+import com.thoughtworks.selenium.DefaultSelenium;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.container.test.api.RunAsClient;
+import org.jboss.arquillian.drone.api.annotation.Drone;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.test.api.ArquillianResource;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.net.URL;
+import java.util.logging.Logger;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: tw
+ * Date: 21.01.14
+ * Time: 12:09
+ * To change this template use File | Settings | File Templates.
+ */
+@RunWith(Arquillian.class)
+public class PetTypeTest {
+
+    private static Logger log = Logger.getLogger(SpecialtiesTest.class.getName());
+
+    @Deployment(testable = false)
+    public static WebArchive createDeployment() {
+        return Deployments.createPetTypeDeployment();
+    }
+
+    @Drone
+    DefaultSelenium driver;
+
+    @ArquillianResource
+    URL deploymentUrl;
+
+    @Test
+    @RunAsClient
+    public void testOpeningHomePage() {
+        String url = deploymentUrl.toExternalForm();
+        log.info("url: "+url);
+        driver.open(url);
+        String pageTitle = driver.getTitle();
+        log.info("pageTitle: " + pageTitle);
+        Assert.assertEquals(pageTitle, "Petclinic");
+    }
+
+    @Test
+    @RunAsClient
+    public void testOpeningPetTypesPage() {
+        String url = deploymentUrl.toExternalForm() + "petTypes.xhtml";
+        log.info("url: " + url);
+        driver.open(url);
+        boolean isPresent = driver.isElementPresent("id=petTypes");
+        log.info("isPresent: " + isPresent);
+        Assert.assertTrue(isPresent);
+    }
+
+    @Test
+    @RunAsClient
+    public void testNewPetTypePage() {
+        String url = deploymentUrl.toExternalForm() + "petTypes.xhtml";
+        log.info("url: "+url);
+        driver.open(url);
+        driver.click("id=petTypesForm:getNewPetTypeForm");
+        driver.waitForPageToLoad("15000");
+        String page = driver.getLocation();
+        log.info("page: "+page);
+        boolean isPresent = driver.isElementPresent("id=addNewPetType");
+        log.info("isPresent: " + isPresent);
+        Assert.assertTrue(isPresent);
+        driver.type("id=addNewPetTypeForm:name","cat");
+        driver.click("id=addNewPetTypeForm:save");
+        driver.waitForPageToLoad("15000");
+        isPresent = driver.isElementPresent("id=petTypes");
+        log.info("isPresent: " + isPresent);
+        Assert.assertTrue(isPresent);
+        isPresent = driver.isElementPresent("xpath=//td[contains(text(), 'cat')]");
+        log.info("isPresent: " + isPresent);
+        Assert.assertTrue(isPresent);
+    }
+
+    @Test
+    @RunAsClient
+    public void testEditPetTypePage() {
+        String url = deploymentUrl.toExternalForm() + "petTypes.xhtml";
+        log.info("url: "+url);
+        driver.open(url);
+        driver.click("id=petTypesForm:petTypesTable:0:edit");
+        driver.waitForPageToLoad("15000");
+        String page = driver.getLocation();
+        log.info("page: "+page);
+        boolean isPresent = driver.isElementPresent("id=editPetType");
+        log.info("isPresent: " + isPresent);
+        Assert.assertTrue(isPresent);
+        driver.type("id=editPetTypeForm:name","dog");
+        driver.click("id=editPetTypeForm:save");
+        driver.waitForPageToLoad("15000");
+        isPresent = driver.isElementPresent("id=petTypes");
+        log.info("isPresent: " + isPresent);
+        Assert.assertTrue(isPresent);
+        isPresent = driver.isElementPresent("xpath=//td[contains(text(), 'dog')]");
+        log.info("isPresent: " + isPresent);
+        Assert.assertTrue(isPresent);
+    }
+
+
+    @Test
+    @RunAsClient
+    public void testDeletePetTypePage() {
+        String url = deploymentUrl.toExternalForm() + "petTypes.xhtml";
+        log.info("url: "+url);
+        driver.open(url);
+        driver.click("id=petTypesForm:petTypesTable:0:delete");
+        driver.waitForPageToLoad("15000");
+        String page = driver.getLocation();
+        log.info("page: "+page);
+        boolean isPresent = driver.isElementPresent("id=petTypes");
+        log.info("isPresent: " + isPresent);
+        Assert.assertTrue(isPresent);
+        isPresent = driver.isElementPresent("xpath=//td[contains(text(), 'dog')]");
+        log.info("isPresent: " + isPresent);
+        Assert.assertFalse(isPresent);
+    }
+}
